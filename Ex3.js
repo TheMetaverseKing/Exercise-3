0b81264acb5ec70a00556e8f00f9e231359e4545
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-03 23:58:04 +0500

    Add files via upload

---
+++ Ex3.js
@@ -0,0 +1,7 @@
+"use strict";
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase
+let personname = "thE meTaVeRsE kINg";
+let lowercasename = personname.toLowerCase();
+let uppercasename = personname.toUpperCase();
+console.log(lowercasename);
+console.log(uppercasename);
---
+++ Ex3.ts
@@ -0,0 +1,8 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase
+
+
+let personname:string = "thE meTaVeRsE kINg";
+let lowercasename : string = personname.toLowerCase();
+let uppercasename : string = personname.toUpperCase();
+console.log(lowercasename);
+console.log(uppercasename);
